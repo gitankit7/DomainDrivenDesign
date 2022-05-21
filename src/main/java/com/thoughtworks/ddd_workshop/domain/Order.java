5400db4173e0c15b04f09ef302d1f700ea2d565d
gitankit7 <reachankit91@gmail.com>
2022-05-21 12:14:34 +0530

    bt|as adding test for cart order thingy

--- src/main/java/com/thoughtworks/ddd_workshop/domain/Order.java
+++ src/main/java/com/thoughtworks/ddd_workshop/domain/Order.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.ddd_workshop.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Order {
     public Order(List<Product> productsList) {
@@ -11,5 +12,18 @@ public class Order {
 
     public List<Product> getCheckedOutCartProducts() {
         return checkedOutCartProducts;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return checkedOutCartProducts.equals(order.checkedOutCartProducts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(checkedOutCartProducts);
     }
 }
--- src/test/java/cart/CartTest.java
+++ src/test/java/cart/CartTest.java
@@ -1,12 +1,10 @@
 package cart;
 
-import com.thoughtworks.ddd_workshop.domain.Cart;
-import com.thoughtworks.ddd_workshop.domain.Item;
-import com.thoughtworks.ddd_workshop.domain.Price;
-import com.thoughtworks.ddd_workshop.domain.Product;
+import com.thoughtworks.ddd_workshop.domain.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.Currency;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -16,7 +14,8 @@ public class CartTest {
   @Test
   public void noItemInTheCart_ItemGetsAddedToCartWhenAddToCartIsCalled() {
     Cart cart = new Cart();
-    cart.addItem(new Item(new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)), 2));
+    cart.addItem(
+        new Item(new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)), 2));
 
     assertTrue(cart.getItems().size() == 1);
     assertTrue(cart.getItems().get(0).getQuantity() == 2);
@@ -25,8 +24,10 @@ public class CartTest {
   @Test
   public void removeItemRemovesItemFromCart() {
     Cart cart = new Cart();
-    cart.addItem(new Item(new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)), 2));
-    cart.addItem(new Item(new Product("Apple Pencil2", new Price(Currency.getInstance("USD"), 50)), 3));
+    cart.addItem(
+        new Item(new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)), 2));
+    cart.addItem(
+        new Item(new Product("Apple Pencil2", new Price(Currency.getInstance("USD"), 50)), 3));
 
     cart.remove(new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)));
 
@@ -36,8 +37,10 @@ public class CartTest {
   @Test
   public void removeItemRemovesItemFromCart_addsItToRemovedList() {
     Cart cart = new Cart();
-    cart.addItem(new Item(new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)), 2));
-    cart.addItem(new Item(new Product("Apple Pencil2", new Price(Currency.getInstance("USD"), 50)), 3));
+    cart.addItem(
+        new Item(new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)), 2));
+    cart.addItem(
+        new Item(new Product("Apple Pencil2", new Price(Currency.getInstance("USD"), 50)), 3));
 
     cart.remove(new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)));
 
@@ -48,12 +51,31 @@ public class CartTest {
   public void compareCartsForEquality() {
     Cart cart1 = new Cart();
     Cart cart2 = new Cart();
-    cart1.addItem(new Item(new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)), 2));
-    cart2.addItem(new Item(new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)), 2));
-
+    cart1.addItem(
+        new Item(new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)), 2));
+    cart2.addItem(
+        new Item(new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)), 2));
 
     assertFalse(cart1.equals(cart2));
     assertTrue(cart1.equals(cart1));
     assertTrue(cart2.equals(cart2));
+  }
+
+  @Test
+  public void checkoutRetursFlattenedListOfProducts() {
+    Cart cart1 = new Cart();
+    cart1.addItem(
+        new Item(new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)), 2));
+    cart1.addItem(new Item(new Product("Apples", new Price(Currency.getInstance("USD"), 50)), 1));
+
+    Order actualResult = cart1.checkout();
+
+    Order expectedResult =
+        new Order(List.of(new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)),
+                new Product("Apple Pencil", new Price(Currency.getInstance("USD"), 50)),
+                new Product("Apples", new Price(Currency.getInstance("USD"), 50))
+                ));
+
+    assertTrue(expectedResult.equals(actualResult));
   }
 }
